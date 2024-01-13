375f49314894177d5332fe0972d72dd0db10dd6e
preetamrawat <psrawat1320@gmail.com>
2024-01-13 22:50:14 +0530

    updated getpost route

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 /* eslint-disable no-empty */
+import { flushSync } from 'react-dom';
 import conf from '../conf/config'
 import {Client, Account, ID, Databases, Storage ,Query} from 'appwrite'
 
@@ -71,18 +72,45 @@ export class Service{
         }
     }
 
-    async getPost(slug){
+    async getPost(queries= [Query.equal("status","active")]){
         try {
-            return await this.databases.getDocument(
+            return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug
+                queries, 
             )
         } catch (error) {
             console.log("Appwrite serive :: getPost :: error", error);
             return false
         }
     }
+
+    // file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: errpr", error);
+            return false
+        }
+    }
+
+    // async deleteFile(fileId){
+    //     try {
+    //         return await this.bucket.deleteFile(
+    //             conf.appwriteBucketId,
+    //             fileId
+    //         )
+    //     } catch (error) {
+    //         console.log("Appwrite serive :: getPost :: error", error);
+    //         return false
+    //     }
+    // }
 }
 
 const service = new Service()
