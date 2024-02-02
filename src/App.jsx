b48896af105f33f8685472c03efded3294388b71
preetamrawat <psrawat1320@gmail.com>
2024-02-02 21:04:37 +0530

    Created container folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
 import {login, logout} from './store/authSlice'
+import { Footer, Header } from './components'
+// import { Outlet } from 'react-router-dom'
 
 function App() {
   const [ loading, setLoading] = useState(true)
@@ -22,8 +24,18 @@ function App() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+ 
+
   return !loading ? (
-    <div className=''></div>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO : {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
   ) : null
 
 }
