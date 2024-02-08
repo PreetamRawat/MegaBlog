e2390ddd5c5f1801b15c1a97fbe8a0abb8cc6120
preetamrawat <psrawat1320@gmail.com>
2024-02-08 20:08:51 +0530

    created an Select component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useId } from 'react'
@@ -8,7 +9,18 @@ const Input =  React.forwardRef(function Input({
   classname='',
   ...props
 },ref){
-  return <div className='w-full'></div>
+  return <div className='w-full'>
+    {label && <label
+    className='inline-block mb-1 pl-1' htmlFor={id}>
+    {label}
+    </label>
+    }
+    <input type={type} className={ `${classname}`}
+      ref={ref}
+      {...props}
+      id={id}
+    />
+  </div>
 })
 
 
